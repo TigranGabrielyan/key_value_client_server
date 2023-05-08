0bac28837e990377c03886bbea5655ffc5190b09
Tigran Gabrielyan <gabrielyan_tigran@yahoo.com>
2023-05-08 15:22:36 +0400

    Add googletest to external dependencies. Implement dummy tests

---
+++ common/test/gtest/client.cc
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(KVM_CLIENT, positive_2_plus_2_equal_4)
+{
+    EXPECT_EQ(4, 2+2);
+}
---
+++ common/test/gtest/server.cc
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(KVM_SERVER, positive_2_plus_3_equal_5)
+{
+    EXPECT_EQ(5, 2+3);
+}
